a2f3e240fe5c4337558a77d465c1f6ee5753bf44
Jesen Tanadi <mail@jesentanadi.com>
2019-08-21 07:06:26 -0400

    move tools to separate file

---
+++ src/dwg/tools.js
@@ -0,0 +1,15 @@
+const drawingTools = {
+  "rect": "1",
+  "ellipse": "2",
+  "line": "3",
+  "polyline": "4",
+  "brush": "5",
+  "text": "6",
+}
+
+const historyTools = {
+  "undo": "↶",
+  "redo": "↷",
+}
+
+export { drawingTools, historyTools }
