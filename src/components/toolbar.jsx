6a0b6e9d2b5885860c9f1c2c6e58b2424b4f6290
Jesen Tanadi <mail@jesentanadi.com>
2019-08-20 21:29:10 -0400

    refactor toolbar, use barItems and bar components

--- src/components/toolbar.jsx
+++ src/components/toolbar.jsx
@@ -1,83 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
+import Bar from "./bar.jsx";
+import BarItems from "./barItems.jsx";
 
-function LeftBar(props) {
-  const { tools, selectedTool, side, handleClick } = props;
-  return (
-    <div className="bar" id="bar-left">{
-      tools.map((tool, i) => {
-        const className = tool === selectedTool
-          ? "tool selected"
-          : "tool"
-
-        return (
-          <div
-            key={ tool }
-            className={ className }
-            id={ tool }
-            onClick={ ev => handleClick(ev.target.id) }
-          >
-            <span className="small">{ i + 1 }</span>
-          </div>
-        )
-      })
-    }</div>
-  )
+function addSelectedClass(target) {
+  target.classList.add("selected");
 }
 
-class RightBar extends Component {
-  constructor() {
-    super()
-    this.addSelected = this.addSelected.bind(this)
-    this.removeSelected = this.removeSelected.bind(this)
-  }
-
-  addSelected(ev) {
-    ev.target.classList.add("selected");
-  }
-  removeSelected(ev) {
-    ev.target.classList.remove("selected");
-  }
-
-  render() {
-    const tools = {
-      "undo": "↶",
-      "redo": "↷",
-    };
-    return (
-      <div className="bar" id="bar-right"> {
-        Object.keys(tools).map(key => (
-          <div
-            key={ key }
-            className="tool"
-            id={ key }
-            onMouseDown={ this.addSelected }
-            onMouseUp={ this.removeSelected }
-            onMouseLeave={ this.removeSelected }
-            onClick={ ev => this.props.handleClick(ev.target.id) }
-          >
-            <span>{ tools[key] }</span>
-          </div>
-        ))
-      }</div>
-    )
-  }
+function removeSelectedClass(target) {
+  target.classList.add("selected");
 }
 
-function Toolbar(props) {
-  const { tools, selectedTool, pickTool, changeHistory } = props;
+export default function Toolbar(props) {
+  const {
+    drawTools,
+    historyTools,
+    selectedTool,
+    pickTool,
+    changeHistory
+  } = props;
 
   return (
     <div id="toolbar-container">
-      <LeftBar
-        tools={ tools }
-        selectedTool={ selectedTool }
-        handleClick={ pickTool }
-      />
-      <RightBar
-        handleClick={ changeHistory }
-      />
+      <Bar side="left">
+        <BarItems
+          items={ drawTools }
+          selectedItem={ selectedTool }
+          itemClass="tool"
+          onClick={ ev => pickTool(ev.target.id) }
+        />
+      </Bar>
+      <Bar side="right">
+        <BarItems
+          items={ historyTools }
+          itemClass="tool"
+          onMouseDown={ ev => addSelectedClass(ev.target) }
+          onMouseUp={ ev => removeSelectedClass(ev.target) }
+          onMouseMove={ ev => removeSelectedClass(ev.target) }
+          onClick={ ev => changeHistory(ev.target.id) }
+        />
+      </Bar>
     </div>
-  )
+  );
 }
-
-export default Toolbar;
