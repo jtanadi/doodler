ea2164f519e9e9fa030586cb1be5a5d4bb91ce8b
Jesen Tanadi <mail@jesentanadi.com>
2020-01-20 18:50:05 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+const port = env.process.PORT || 3000
+
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"))
+})
+
+app.listen(port, () => console.log(`Using port ${port}`))
