cf28922f165728f92ba79da0e1d0599b28cfad51
Jesen Tanadi <mail@jesentanadi.com>
2020-01-20 18:52:46 -0500

    fix port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 
 const app = express()
-const port = env.process.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "dist/index.html"))
