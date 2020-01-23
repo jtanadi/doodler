6bbe63bfb9410cd7ea6868dc556d459224183327
Jesen Tanadi <mail@jesentanadi.com>
2020-01-23 06:53:17 -0500

    remove unimplemented tools

--- src/dwg/tools.js
+++ src/dwg/tools.js
@@ -1,10 +1,17 @@
+// const drawingTools = {
+//   "rect": "1",
+//   "ellipse": "2",
+//   "line": "3",
+//   "polyline": "4",
+//   "brush": "5",
+//   "text": "6",
+// }
+
 const drawingTools = {
   "rect": "1",
   "ellipse": "2",
   "line": "3",
-  "polyline": "4",
-  "brush": "5",
-  "text": "6",
+  "brush": "4",
 }
 
 const historyTools = {
