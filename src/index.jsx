58f6687fe2c6e1b89bac3d88a1483f86f20ad104
Jesen Tanadi <mail@jesentanadi.com>
2020-01-22 23:12:26 -0500

    initial commit; move App to component

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/NewApp"
+
+const root = document.getElementById("root");
+ReactDOM.render(<App />, root);
+
