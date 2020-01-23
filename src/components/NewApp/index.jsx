5964b663ada9e673ec46aa7e8481295a026b5d80
Jesen Tanadi <mail@jesentanadi.com>
2020-01-22 23:13:13 -0500

    add temp App for testing

---
+++ src/components/NewApp/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import ToolPalette from "../ToolPalette"
+import StyledDiv from "./styled"
+
+export default function App() {
+  return (
+    <StyledDiv>
+      <ToolPalette />
+    </StyledDiv>
+  )
+}
