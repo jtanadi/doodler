3c3be3a6e60d39d94e66d8dbdd36b1b456464603
Jesen Tanadi <mail@jesentanadi.com>
2020-02-11 06:40:32 -0500

    use MouseEvent instead of SyntheticEvent

--- src/components/ToolPaletteBar/index.tsx
+++ src/components/ToolPaletteBar/index.tsx
@@ -1,8 +1,8 @@
-import React, { ReactElement, SyntheticEvent } from "react"
+import React, { ReactElement, MouseEvent } from "react"
 import { StyledBar, StyledMinimizeBtn } from "./styles"
 
 type PropTypes = {
-  onClick(e: SyntheticEvent): void
+  onClick(e: MouseEvent): void
 }
 
 const ToolPaletteBar: React.FC<PropTypes> = ({ onClick }): ReactElement => {
