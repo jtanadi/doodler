55f37207ca047adc805728b23983eee07a1f1a43
Jesen Tanadi <mail@jesentanadi.com>
2020-02-12 08:49:52 -0500

    initial commit: move bounding box style to module

---
+++ src/utils/drawingStyles.ts
@@ -0,0 +1,13 @@
+const boundingBoxStyle = {
+  edgeStyle: {
+    strokeColor: "#0D98BA",
+    strokeWidth: 2,
+  },
+  nodeStyle: {
+    strokeColor: "black",
+    strokeWidth: 1,
+    fillColor: "white",
+  },
+}
+
+export { boundingBoxStyle }
